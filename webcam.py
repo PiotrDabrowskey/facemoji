93ef4b76f7cc7d3813905f3c8ab23144013b4078
Miszu Zolnieruk <gierekg@gmail.com>
2016-05-10 01:02:37 +0200

    Added emotion recognition and simple dataset

---
+++ webcam.py
@@ -0,0 +1,88 @@
+import cv2
+import glob
+import random
+import numpy as np
+
+emotions = ["happy", "sad"] #Emotion list
+fishface = cv2.createFisherFaceRecognizer() #Initialize fisher face classifier
+
+data = {}
+
+def get_files(emotion): #Define function to get file list, randomly shuffle it and split 80/20
+    files = glob.glob("processed/%s/*" %emotion)
+    random.shuffle(files)
+    training = files[:int(len(files)*0.8)] #get first 80% of file list
+    prediction = files[-int(len(files)*0.2):] #get last 20% of file list
+    return training, prediction
+
+def make_sets():
+    training_data = []
+    training_labels = []
+    prediction_data = []
+    prediction_labels = []
+    for emotion in emotions:
+        training, prediction = get_files(emotion)
+        #Append data to training and prediction list, and generate labels 0-7
+        for item in training:
+            image = cv2.imread(item) #open image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert to grayscale
+            training_data.append(gray) #append image array to training data list
+            training_labels.append(emotions.index(emotion))
+
+        for item in prediction: #repeat above process for prediction set
+            image = cv2.imread(item)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            prediction_data.append(gray)
+            prediction_labels.append(emotions.index(emotion))
+
+    return training_data, training_labels, prediction_data, prediction_labels
+
+def run_recognizer():
+    training_data, training_labels, prediction_data, prediction_labels = make_sets()
+
+    print "training fisher face classifier"
+    print "size of training set is:", len(training_labels), "images"
+    fishface.train(training_data, np.asarray(training_labels))
+
+    print "predicting classification set"
+    cnt = 0
+    correct = 0
+    incorrect = 0
+    for image in prediction_data:
+        pred, conf = fishface.predict(image)
+        if pred == prediction_labels[cnt]:
+            correct += 1
+            cnt += 1
+        else:
+            incorrect += 1
+            cnt += 1
+    return ((100 * correct) / (correct + incorrect))
+
+def transform_webcam():
+    # initialize the camera
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened():  # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+    cv2.destroyWindow("preview")
+
+# Now run it
+metascore = []
+for i in range(0, 10):
+    correct = run_recognizer()
+    print "got", correct, "percent correct!"
+    metascore.append(correct)
+
+print "\n\nend score:", np.mean(metascore), "percent correct!"
+
+transform_webcam()
