64d8f62d68d0fdabdcd12144319584735efcc66d
Piotr Dąbrowski <piotr.dabrowski.sbk@gmail.com>
2016-05-10 05:34:18 +0200

    webcam emotion detection, draw graphics over face

--- face_detect.py
+++ face_detect.py
@@ -15,18 +15,16 @@ def detect_faces(image):
     return faces  # list of (x, y, w, h)
 
 
-def detect_face_and_crop(image):
-    faces = detect_faces(image)
-    if len(faces) == 0:
+def crop_face(image, x, y, h, w):
+    if w == 0 or h == 0 or y + h > image.shape[0] or x + w > image.shape[1]:
         return None
-    (x, y, w, h) = faces[0]
-    if w == 0 or h == 0 or y+h > image.shape[0] or x+w > image.shape[1]:
-        return None
-    return image[y:y+h, x:x+w]
+    return image[y:y + h, x:x + w]
 
 
 if __name__ == "__main__":
     image = cv2.imread('data/happy/139,131,192,128.jpg')
-    cropped = detect_face_and_crop(image)
+    faces = detect_faces(image)
+    (x, y, w, h) = faces[0]
+    cropped = crop_face(image, x, y, w, h)
     cv2.imshow("cropped", cropped)
     cv2.waitKey(0)
--- webcam.py
+++ webcam.py
@@ -2,31 +2,40 @@ import Image
 
 import cv2
 
-from face_detect import detect_face_and_crop
+from face_detect import detect_faces, crop_face
 from image_converters import nparray_as_image, image_as_nparray
 
 
-def show_webcam_and_detect_face():
+def show_webcam_and_run(model):
     cv2.namedWindow('webcam')
     cv2.namedWindow('face_preview')
-    vc = cv2.VideoCapture(0)
 
-    if vc.isOpened():  # try to get the first frame
-        rval, frame = vc.read()
-    else:
-        rval = False
-        frame = None
+    happy_img = nparray_as_image(cv2.imread('graphics/happy.png'))
+    sad_img = nparray_as_image(cv2.imread('graphics/sad.png'))
+
+    vc = cv2.VideoCapture(0)
+    rval, frame = vc.read() if vc.isOpened() else None  # try to get the first frame
 
     while rval:
+        faces = detect_faces(frame)
+        if len(faces) > 0:
+            (x, y, w, h) = faces[0]
+            cropped = crop_face(frame, x, y, w, h)
+            if cropped is not None:  # if face detected
+                image = nparray_as_image(cropped)  # convert to image
+                image = image.resize((200, 200), Image.ANTIALIAS)  # reize to model's input
+                cropped = image_as_nparray(image)  # convert back to nparray
+                gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+                prediction = model.predict(gray)  # do prediction
+                print prediction
+                cv2.imshow('face_preview', cropped)  # face visualization
+
+                img_to_draw = happy_img if prediction[0] == 1 else sad_img  # select emotion's image
+                img_to_draw = img_to_draw.resize((h, w), Image.ANTIALIAS)  # resize to face's size
+                emotion = image_as_nparray(img_to_draw)  # convert to nparray
+                frame[y:y + h, x:x + h] = emotion
+
         cv2.imshow('webcam', frame)
-
-        face_from_frame = detect_face_and_crop(frame)
-        if face_from_frame is not None:
-            image = nparray_as_image(face_from_frame)
-            image = image.resize((200, 200), Image.ANTIALIAS)
-
-            face_from_frame = image_as_nparray(image)
-            cv2.imshow('face_preview', face_from_frame)
 
         rval, frame = vc.read()
         key = cv2.waitKey(1)
@@ -42,4 +51,4 @@ if __name__ == '__main__':
     fisher_face.load('models/emotion_detection_model.xml')
 
     # use learnt model
-    show_webcam_and_detect_face()
+    show_webcam_and_run(fisher_face)
