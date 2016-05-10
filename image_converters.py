d6330859e0b223095fe06bec80a5fd8e46c7fcbb
Piotr Dąbrowski <piotr.dabrowski.sbk@gmail.com>
2016-05-10 05:48:41 +0200

    image converters - added parameter mode

--- image_converters.py
+++ image_converters.py
@@ -6,5 +6,5 @@ def image_as_nparray(image):
     return np.asarray(image)
 
 
-def nparray_as_image(nparray):
-    return Image.fromarray(np.asarray(np.clip(nparray, 0, 255), dtype='uint8'), 'RGB')
+def nparray_as_image(nparray, mode='RGB'):
+    return Image.fromarray(np.asarray(np.clip(nparray, 0, 255), dtype='uint8'), mode)
