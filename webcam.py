c36d45f1b397a8e0be8ab5849c280ab817bfa93e
Piotr Dąbrowski <piotr.dabrowski.sbk@gmail.com>
2016-05-10 05:48:13 +0200

    webcam draw emotion - fixed transparency

--- webcam.py
+++ webcam.py
@@ -10,8 +10,8 @@ def show_webcam_and_run(model):
     cv2.namedWindow('webcam')
     cv2.namedWindow('face_preview')
 
-    happy_img = nparray_as_image(cv2.imread('graphics/happy.png'))
-    sad_img = nparray_as_image(cv2.imread('graphics/sad.png'))
+    happy_img = nparray_as_image(cv2.imread('graphics/happy.png', -1), mode=None)
+    sad_img = nparray_as_image(cv2.imread('graphics/sad.png', -1), mode=None)
 
     vc = cv2.VideoCapture(0)
     rval, frame = vc.read() if vc.isOpened() else None  # try to get the first frame
@@ -27,13 +27,14 @@ def show_webcam_and_run(model):
                 cropped = image_as_nparray(image)  # convert back to nparray
                 gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)  # convert to grayscale
                 prediction = model.predict(gray)  # do prediction
-                print prediction
                 cv2.imshow('face_preview', cropped)  # face visualization
 
                 img_to_draw = happy_img if prediction[0] == 1 else sad_img  # select emotion's image
                 img_to_draw = img_to_draw.resize((h, w), Image.ANTIALIAS)  # resize to face's size
                 emotion = image_as_nparray(img_to_draw)  # convert to nparray
-                frame[y:y + h, x:x + h] = emotion
+                for c in range(0, 3):
+                    frame[y:y + h, x:x + w, c] = emotion[:, :, c] * (emotion[:, :, 3] / 255.0) \
+                                                 + frame[y:y + h, x:x + w, c] * (1.0 - emotion[:, :, 3] / 255.0)
 
         cv2.imshow('webcam', frame)
 
